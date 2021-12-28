dbdd246c81d41cf1712d37be769d8f570ac598af
z emmanuel <zainy0012@gmail.com>
2021-12-28 10:50:21 -0500

    added a second one pass approach
    added a second, refactored code solution to simply traverse the tree in one pass and be able to produce the answer.
    the left and right subtrees are processed separately, the root is never traversed. o(n) time, o(n) space

--- boundarytraversal.py
+++ boundarytraversal.py
@@ -45,3 +45,40 @@ class Solution:
                 preorder.append(node.left);
         
         return [root.val] + left[::-1] + bottom + right;
+
+'''
+VERSION 2: ONE PASS! NO CHILD-PARENT RELATIONSHIP NEEDED
+'''
+
+        def findFirstLeafV2(self, node, direction, leftboundary, midleafs, rightboundary):
+                    if not node:
+                        return;
+
+                    if direction < 0 and not self.firstLeftLeaf:
+                        leftboundary.append(node.val);
+                    elif direction > 0 and not self.firstRightLeaf:
+                        rightboundary.append(node.val);
+
+                    if not node.left and not node.right:
+                        if direction < 0 and not self.firstLeftLeaf:
+                            self.firstLeftLeaf = node;
+                        elif direction > 0 and not self.firstRightLeaf:
+                            self.firstRightLeaf = node;
+                        else:
+                            midleafs.append(node.val);
+                        return;
+
+                    self.findFirstLeafV2(node.left if direction < 0 else node.right, direction, leftboundary, midleafs, rightboundary);
+                    self.findFirstLeafV2(node.right if direction < 0 else node.left, direction, leftboundary, midleafs, rightboundary);
+
+
+    def boundaryOfBinaryTreeV2(self, root):
+        if not root:
+            return [];
+        left = [];
+        leftmidleafs = [];
+        rightmidleafs = [];
+        right = [];
+        self.findFirstLeafV2(root.left, -1, left, leftmidleafs, right);
+        self.findFirstLeafV2(root.right, 1, left, rightmidleafs, right);
+        return [root.val] + left + leftmidleafs + rightmidleafs[::-1] + right[::-1];
